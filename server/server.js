6c6d7d27e7f8a326ddbb638e0b83f12006b5ecb8
bar ziv <barziv65@gmail.com>
2020-07-13 20:41:39 +0300

    added db manager injecion

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-const PostManager = require('./posts/postsManager');
+const PostsManager = require('./posts/postsManager');
+const PostsValidation = require('./validation/postsValidation');
+const MongoManager = require('./DbManager/mongoDbManager');
 const express = require('express')
 const bodyParser = require("body-parser");
 const multer = require('multer');
@@ -6,6 +8,7 @@ const upload = multer();
 var cors = require('cors');
 const app = express()
 const port = 4000
+const postsManager = new PostsManager(new PostsValidation(), new MongoManager());
 
 app.use(cors());
 app.use(bodyParser.urlencoded({
@@ -17,11 +20,11 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use("/pictures", express.static(__dirname+"/pictures"));
 
 app.get('/posts', (req, res) => {
-    res.send(PostManager.getAllPosts())
+    res.send(postsManager.getAllPosts())
 })
 
 app.post('/posts', upload.single('picture'), (req, res) => {
-    if (PostManager.uploadNewPost(req.body, req.file)) {
+    if (postsManager.uploadNewPost(req.body, req.file)) {
         res.send('new post set');
     }
     else {
@@ -30,7 +33,7 @@ app.post('/posts', upload.single('picture'), (req, res) => {
 })
 
 app.put('/posts', (req, res) => {
-    if (PostManager.changeLikes(req.body.id, req.body.isLike)) {
+    if (postsManager.changeLikes(req.body.id, req.body.isLike)) {
         res.send('new post set');
     }
     else {
@@ -39,7 +42,7 @@ app.put('/posts', (req, res) => {
 })
 
 app.delete('/posts/:id', (req, res) => {
-    res.send(PostManager.deletePost(req.params.id));
+    res.send(postsManager.deletePost(req.params.id));
 })
 
 app.listen(port, () => console.log(`instabar app listening at http://localhost:${port}`))
