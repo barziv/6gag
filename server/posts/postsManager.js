69d924c14d6678a84f9832ef464db1670b217917
bar ziv <barziv65@gmail.com>
2020-07-13 13:12:19 +0300

    postManager save photos and new posts

--- server/posts/postsManager.js
+++ server/posts/postsManager.js
@@ -1,8 +1,14 @@
 const uuid = require('uuid');
+const fs = require('fs');
 
 class PostsManager {
     constructor() {
         this.posts = new Map();
+        let dirPath = __dirname+"/../pictures";
+
+        if (!fs.existsSync(dirPath)){
+            fs.mkdirSync(dirPath);
+        }
     }
 
     getAllPosts() {
@@ -13,15 +19,27 @@ class PostsManager {
         return this.posts.get(id);
     }
 
-    uploadNewPost(postInformation) {
+    getPicture(id) {
+        return this.pictures.get(id);
+    }
+
+    uploadNewPost(postInformation, picture) {
         if ("ownerID" in postInformation && postInformation.ownerID !== "") {
             let id = uuid.v4();
             postInformation["id"] = id;
             this.posts.set(id, postInformation);
+            this._saveToFile(id, picture);
             return true;
         }
 
         return false;
+    }
+
+    _saveToFile(filename, file) {
+        let filePath = __dirname+"/../pictures/"+filename;
+        fs.appendFile(filePath, file.buffer, () => {
+            console.log("write to file");
+        });
     }
 
 }
