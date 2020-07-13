0ad683e6cd6d9a9e37ab63ca424d69fa08bd5c68
bar ziv <barziv65@gmail.com>
2020-07-13 13:07:25 +0300

    server get new post request

--- server/server.js
+++ server/server.js
@@ -1,26 +1,29 @@
 const PostManager = require('./posts/postsManager');
 const express = require('express')
 const bodyParser = require("body-parser");
+const multer = require('multer');
+const upload = multer();
 var cors = require('cors');
 const app = express()
 const port = 4000
 
 app.use(cors());
 app.use(bodyParser.urlencoded({
+    limit: '50mb',
     extended: true
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'})); 
+
+app.use("/pictures", express.static(__dirname+"/pictures"));
 
 app.get('/posts', (req, res) => {
     console.log("posts req");
     res.send(PostManager.getAllPosts())
 })
 
-app.get('/posts/:id', (req, res) => res.send(PostManager.getSpecificPost(req.params.id)))
+app.post('/posts', upload.single('picture'), (req, res) => {
 
-app.post('/posts', (req, res) => {
-    console.log(req.body);
-    if (PostManager.uploadNewPost(req.body)) {
+    if (PostManager.uploadNewPost(req.body, req.file)) {
         res.send('new post set');
     }
     else {
