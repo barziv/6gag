1b6a7c1dc5bc2c2d2b42caf801c79f6445b6d7af
bar ziv <barziv65@gmail.com>
2020-07-14 22:19:10 +0300

    send new comment to the server

---
+++ client/client/src/postsComponnent/commentsComponnent/inputComponnent.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Grid from '@material-ui/core/Grid';
+import config from '../../config';
+
+function sendMessage(newMessage, id, changeComments) {
+    let data = {
+        id,
+        comment: newMessage
+    };
+
+    changeComments(newMessage);
+    fetch(config.SERVER_ADDRESS+'/posts', {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+}
+
+export default function Input(props) {
+    const [newMessage, setNewMessage] = useState('');
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={9}>
+                <TextField fullWidth id="outlined-basic" label="Message" variant="outlined" 
+                    onChange={(event) => setNewMessage(event.target.value)}/>
+            </Grid>
+            <Grid item xs={2}>
+                <Fab color="primary" aria-label="add" 
+                onClick={() => sendMessage(newMessage, props.id, props.changeComments)}>
+                    <AddIcon />
+                </Fab>
+            </Grid>
+        </Grid>
+    );
+}
