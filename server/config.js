51e5fc891f7f05f8de96a1bb71cd25d69aa32f2e
bar ziv <barziv65@gmail.com>
2020-07-14 12:58:12 +0300

    config file added

---
+++ server/config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.DB_NAME = "InstaBar";
+config.COLLECTION_NAME = "posts";
+config.MONGODB_URL = "mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb";
+
+module.exports = config;
