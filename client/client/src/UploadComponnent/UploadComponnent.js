3e3b647f957d3570880f0b0841f3f1c1ab9fed77
bar ziv <barziv65@gmail.com>
2020-07-14 18:39:50 +0300

    type of post update added

--- client/client/src/UploadComponnent/UploadComponnent.js
+++ client/client/src/UploadComponnent/UploadComponnent.js
@@ -1,53 +1,65 @@
-import React from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import PublishIcon from '@material-ui/icons/Publish';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import config from '../config';
+import React from "react";
+import Dialog from "@material-ui/core/Dialog";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import PublishIcon from "@material-ui/icons/Publish";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import config from "../config";
 
 function SendNewPostToServer(data) {
-    fetch(config.SERVER_ADDRESS+'/posts', {
-        method: 'POST',
-        mode: 'cors',
-        body: data,
-      });
+  fetch(config.SERVER_ADDRESS + "/posts", {
+    method: "POST",
+    mode: "cors",
+    body: data
+  });
 }
 
 function UploadNewPost(props) {
-    let data = new FormData();
-    const addData = (key, value) => {
-        data.set(key, value);
-    }
+  let data = new FormData();
+  const addData = (key, value) => {
+    data.set(key, value);
+  };
 
   return (
     <div>
-      <Dialog open={props.isOpen} onClose={props.handleClose} aria-labelledby="form-dialog-title" componnet="form">
+      <Dialog
+        open={props.isOpen}
+        onClose={props.handleClose}
+        aria-labelledby="form-dialog-title"
+        componnet="form"
+      >
         <Paper component="form">
-        <DialogTitle id="form-dialog-title">Upload new post</DialogTitle>
-        <InputBase
+          <DialogTitle id="form-dialog-title">Upload new post</DialogTitle>
+          <InputBase
             placeholder="Name"
-            inputProps={{ 'aria-label': 'name' }}
-            onChange={(event) => addData("ownerID", event.target.value)}
-        />
-        <InputBase
+            inputProps={{ "aria-label": "name" }}
+            onChange={event => addData("ownerID", event.target.value)}
+          />
+          <InputBase
             placeholder="Title"
-            inputProps={{ 'aria-label': 'title' }}
-            onChange={(event) => addData("header", event.target.value)}
-        />
-        <InputBase
+            inputProps={{ "aria-label": "title" }}
+            onChange={event => addData("header", event.target.value)}
+          />
+          <InputBase
             placeholder="Description"
-            inputProps={{ 'aria-label': 'description' }}
-            onChange={(event) => addData("description", event.target.value)}
-        />
-        <input type="file" onChange={(event) => addData("picture", event.target.files[0]) }></input>
-        <IconButton type="submit" aria-label="search" onClick={() => {
-            SendNewPostToServer(data);
-            props.handleClose();
-        }}>
+            inputProps={{ "aria-label": "description" }}
+            onChange={event => addData("description", event.target.value)}
+          />
+          <input
+            type="file"
+            onChange={event => addData("picture", event.target.files[0])}
+          ></input>
+          <IconButton
+            type="submit"
+            aria-label="search"
+            onClick={() => {
+              SendNewPostToServer(data);
+              props.handleClose();
+            }}
+          >
             <PublishIcon />
-        </IconButton>
+          </IconButton>
         </Paper>
       </Dialog>
     </div>
--- server/config.js
+++ server/config.js
@@ -3,6 +3,9 @@ var config = {};
 config.DB_NAME = "InstaBar";
 config.COLLECTION_NAME = "posts";
 config.MONGODB_URL = "mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb";
-config.FIELDS_TO_INIT = ["likes"];
+config.QUERYS = {
+    isLike: { command: "$set", field: "isLike", key: "likes" },
+    comment: { command: "$push", field: "comment", key: "comments" }
+};
 
 module.exports = config;
