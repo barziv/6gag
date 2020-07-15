e21cbd70aa90d31087ce581302c8fde384a84c36
bar ziv <barziv65@gmail.com>
2020-07-15 12:48:08 +0300

    create mongo client in private function

--- server/DbManager/mongoDbManager.js
+++ server/DbManager/mongoDbManager.js
@@ -8,66 +8,64 @@ class MongoDbManager {
     }
 
     getByID(id, filter=NaN) {
-        return new Promise((resolve, reject) => { 
-            this.MongoClient.connect(this.url).then(function(db){
-                let dbo = db.db(config.DB_NAME);
-                dbo.collection(config.COLLECTION_NAME).findOne({_id: new mongo.ObjectId(id)}, function(err, res) {
+        return new Promise((resolver, reject) => { 
+            this._getMongoCollection().then((collection) => {
+                collection.findOne({_id: new mongo.ObjectId(id)}, function(err, res) {
                     resolve(res);
-                    db.close();
                 });
             });
         });
     }
 
     getAll() {
-        return new Promise((resolve, reject) => { 
-            this.MongoClient.connect(this.url).then(function(db){
-                var dbo = db.db(config.DB_NAME);
-                dbo.collection(config.COLLECTION_NAME).find({}).toArray(function(err, res) {
-                    resolve(res);
-                    db.close();
+        return new Promise((resolver, reject) => { 
+            this._getMongoCollection().then((collection) => {
+                collection.find({}).toArray(function(err, res) {
+                    resolver(res);
                 });
             });
         });
     }
 
     insert(data) {
-        return new Promise((resolve, reject) => { 
-            this.MongoClient.connect(this.url).then(function(db){
-                var dbo = db.db(config.DB_NAME);
-                dbo.collection(config.COLLECTION_NAME).insertOne(data, function(err, res) {
+        return new Promise((resolver, reject) => { 
+            this._getMongoCollection().then((collection) => {
+                collection.insertOne(data, function(err, res) {
                     if (err) throw err;
                     console.log("1 document inserted");
-                    resolve(res.ops[0]["_id"]);
-                    db.close();
+                    resolver(res.ops[0]["_id"]);
                 });
             });
         });
     }
 
     update(id, data) {
-        this.MongoClient.connect(this.url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db(config.DB_NAME);
-            var myquery = { _id: new mongo.ObjectId(id) };
-            var newvalues = { $set: data };
-            dbo.collection(config.COLLECTION_NAME).updateOne(myquery, newvalues, function(err, res) {
-              if (err) throw err;
-              console.log("1 document updated");
-              db.close();
+        this._getMongoCollection().then((collection) => {
+            let myquery = { _id: new mongo.ObjectId(id) };
+            let newvalues = { $set: data };
+            collection.updateOne(myquery, newvalues, function(err, res) {
+                if (err) throw err;
+                console.log("1 document updated");
             });
-          });
+        });
     }
 
     delete(id) {
+        return new Promise((resolver, reject) => { 
+            this._getMongoCollection().then((collection) => {
+                collection.deleteOne({_id: new mongo.ObjectId(id)}, function(err, res) {
+                    console.log("1 document deleted");
+                    (res.result.n === 1) ? resolver(true) : resolver(false);
+                });
+            });
+        });
+    }
+
+    _getMongoCollection() {
         return new Promise((resolve, reject) => { 
             this.MongoClient.connect(this.url).then(function(db){
-                var dbo = db.db(config.DB_NAME);
-                dbo.collection(config.COLLECTION_NAME).deleteOne({_id: new mongo.ObjectId(id)}, function(err, res) {
-                    console.log("1 document deleted");
-                    (res.result.n === 1) ? resolve(true) : resolve(false);
-                    db.close();
-                });
+                let dbo = db.db(config.DB_NAME);
+                resolve(dbo.collection(config.COLLECTION_NAME), db);
             });
         });
     }
