af702e8ab3d670833ff130be2fec2e15a5d20b03
bar ziv <barziv65@gmail.com>
2020-07-16 18:32:05 +0300

    routing for login page added

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
@@ -6,6 +6,8 @@ import Posts from './postsComponnent/postsComponnent';
 import SmartPost from './postsComponnent/postComponnent/smartPostComponnent';
 import MainPage from './mainPageComponnent';
 import ErrorPage from './errorComponnent';
+import LoginSystem from './loginSystemComponnents/loginSystemComponnent';
+import config from './config';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -22,27 +24,30 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
-
   return (
-    <BrowserRouter>
-      <AppBar position="sticky">
-        <Toolbar>
-          <Link to="/" className={classes.link}>
-            <Button color="inherit" className={classes.menuButton}>Posts</Button>
-          </Link>
-          <Link to="/top-ten" className={classes.link}>
-            <Button color="inherit" className={classes.menuButton}>Top-ten</Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-      <Switch>
-        <Route component={() => <MainPage url={"/posts"}/>} exact path={"/"}/>
-        <Route component={() => <Posts url={"/posts/top-ten"} />} exact path={"/top-ten"}/>
-        <Route component={SmartPost} exact path={"/posts/:id"}/>
-        <Route component={ErrorPage} path={"/"}/>      
-      </Switch>
-    </BrowserRouter>
+    <LoginSystem />
   );
+
+  // return (
+  //   <BrowserRouter>
+  //     <AppBar position="sticky">
+  //       <Toolbar>
+  //         <Link to="/" className={classes.link}>
+  //           <Button color="inherit" className={classes.menuButton}>Posts</Button>
+  //         </Link>
+  //         <Link to="/top-ten" className={classes.link}>
+  //           <Button color="inherit" className={classes.menuButton}>Top-ten</Button>
+  //         </Link>
+  //       </Toolbar>
+  //     </AppBar>
+  //     <Switch>
+  //       <Route component={() => <MainPage url={"/posts"}/>} exact path={"/"}/>
+  //       <Route component={() => <Posts url={"/posts/top-ten"} />} exact path={"/top-ten"}/>
+  //       <Route component={SmartPost} exact path={"/posts/:id"}/>
+  //       <Route component={ErrorPage} path={"/"}/>
+  //     </Switch>
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;
